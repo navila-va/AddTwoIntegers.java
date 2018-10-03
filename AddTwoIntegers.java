1b61b0206af94c2cbe110ed229f7ab4d3c8b42ad
navila-va <43426301+navila-va@users.noreply.github.com>
2018-10-03 03:11:52 +0000

    Create AddTwoIntegers.java

---
+++ AddTwoIntegers.java
@@ -0,0 +1,20 @@
+public class AddTwoIntegers {public static void main(String[] args) {
+
+    
+
+    int first = 20;
+
+    int second = 20;
+
+
+
+    int sum = first + second;
+
+
+
+    System.out.println("The sum is: " + sum);
+
+}
+
+
+}
