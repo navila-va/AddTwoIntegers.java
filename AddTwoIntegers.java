3d74ff05828c583d1d5358dac745c9dcc7d684bb
navila-va <43426301+navila-va@users.noreply.github.com>
2018-10-04 00:33:27 -0400

    Update AddTwoIntegers.java

--- AddTwoIntegers.java
+++ AddTwoIntegers.java
@@ -3,14 +3,16 @@ public class AddTwoIntegers {public static void main(String[] args) {
     
 
     int first = 20;
-
     int second = 20;
-
-    float first = 2.5;
-     float first = 2.5;
-
     int sum = first + second;
+
+    float first = 1.5f;
+    float second = 2.0f;
+
     float product = first * second;
+
+    
+    
 
     System.out.println("The sum is: " + sum);
     System.out.println("The product is: " + product);
